9d7928006afec4d95584df3fb50ca19cbd7ff9ba
Sandking <Sandking@outlook.com>
2020-07-06 20:17:44 -0700

    Perfex WebView - Swift

--- markndo/ViewController.swift
+++ markndo/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWebsite()
+    }
+    
+    private func loadWebsite() {
+        let webURL = URL(string: "http://jonathane3.sg-host.com/crm")
+        let requestURL = URLRequest(url: webURL!)
+        webView.load(requestURL)
+        
+        webView.addSubview(activity)
+        activity.startAnimating()
+        webView.navigationDelegate = self
+        activity.hidesWhenStopped = true
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        activity.startAnimating()
     }
 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activity.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activity.stopAnimating()
+    }
 
 }
 
